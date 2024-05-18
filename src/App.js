8af10f235b3ed52dbbc8ecb2d1484f81b2b6f719
luantranthannh <luan.tran25@student.passerellesnumeriques.org>
2024-05-18 20:54:35 +0700

    Create About and NotFound components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 function App() {
   const [users, setUsers] = useState([]);
   useEffect(() => {
@@ -20,13 +23,22 @@ function App() {
   }, []);
   return (
     <div className="App">
-      <Navbar />
-      <div className="container">
-        <h1>GitHub Users data</h1>
-        {/* <Users users={users} /> */}
-        <Search />
-      </div>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <h1>GitHub Users Data</h1>
+          <Switch>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/about" component={About} />
+            <Route path="/*" component={NotFound}></Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
 export default App;
+{
+  /* <Users users={users} /> */
+}
+//  <Search />
