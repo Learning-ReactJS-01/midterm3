6c7f7144e0b71c07cbc1289dc7f24ebeb7a3acc3
luantranthannh <luan.tran25@student.passerellesnumeriques.org>
2024-05-18 17:26:58 +0700

    Create Users and UserItem component to display all information

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import React from 'react';
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
 
 function App() {
- 
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://api.github.com/users");
+        setUsers(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <Navbar />
       <div className="container">
-        <h1>Hello from React</h1>
+        <h1>Git Hub User data</h1>
+        <Users users={users} />
       </div>
     </div>
   );
