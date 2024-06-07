eb7b174d6c60229b6a70ba445e6bd55cd7086be8
luantranthannh <luan.tran25@student.passerellesnumeriques.org>
2024-06-07 15:41:04 +0700

    delete function fetch user in App.js

--- src/App.js
+++ src/App.js
@@ -5,21 +5,9 @@ import { useEffect, useState } from "react";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/layout/Home";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { getUser} from "./components/data/api";
 function App() {
   const [theme, setTheme] = useState('light');
-
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("https://api.github.com/users");
-        setUsers(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData();
-  }, []);
   return (
     <div className="App">
       <Router>
